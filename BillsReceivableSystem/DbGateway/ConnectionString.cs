b5969fee482f68d52818b0077d04fbfe86d6e601
kowsar11035 <kowsar11035@gmail.com>
2017-02-11 20:20:04 +0600

    UI design for bill receivable entry

---
+++ BillsReceivableSystem/DbGateway/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillsReceivableSystem.DbGateway
+{
+    class ConnectionString
+    {
+        public string DBConn = @"Data Source=KYOTO-PC13;Initial Catalog=BillsReceivableDB;User ID=sa;Password=SystemAdministrator;Persist Security Info=True";
+            
+    }
+}
