8e2d0f850b84fa584483af2f8e025d2de425080f
Ikramunnesa <ikraafi@gmail.com>
2017-08-20 12:03:31 +0600

    reports ui added

---
+++ BillsReceivableSystem/Report/ReportsUI.cs
@@ -0,0 +1,61 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BillsReceivableSystem.Report
+{
+    public partial class ReportsUI : Form
+    {
+        public ReportsUI()
+        {
+            InitializeComponent();
+        }
+
+        private void InReportButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ReportForInvoice f2 = new ReportForInvoice();
+            this.Visible = false;
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void AllReceiBillListButton_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "BillsReceivableDBDemo";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            AllReceivableBillList cr = new AllReceivableBillList();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
+    }
+}
